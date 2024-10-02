ce54b26c12a3286fd186e45e1583c118b4f0c0d1
Urielox <43377310+Urielox@users.noreply.github.com>
2024-10-01 19:10:33 -0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.ui.UIMenu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //UIMenu.showMenu();
+
+        Doctor myDoctor = new Doctor("Uriel","Pedriatria");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
+
+        Patient myPatient = new Patient("Alejandra", "alejandra@gmail.com");
+        System.out.println(myPatient.getweight());
+
     }
 }
