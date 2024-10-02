74a77e9aa8f01124d87f8995e4558483e3d17086
Urielox <43377310+Urielox@users.noreply.github.com>
2024-10-01 19:11:09 -0600

    Update Patient.java

--- src/main/java/org/example/Patient.java
+++ src/main/java/org/example/Patient.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public class Patient {
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String birthday;
+    private double weight;
+    private double height;
+    private String blood;
+
+    Patient(String name, String email){
+        this.name = name;
+        this.email = email;
+        this.weight = 54.5;
+    }
+    public String getweight() {
+        return this.weight + " kg";
+    }
 }
