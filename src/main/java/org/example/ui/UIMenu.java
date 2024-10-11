f5099d5ae32cfb1b363cd633cb687c0b375aab14
Urielox <43377310+Urielox@users.noreply.github.com>
2024-10-10 20:20:17 -0600

    Update UIMenu.java

--- src/main/java/org/example/ui/UIMenu.java
+++ src/main/java/org/example/ui/UIMenu.java
@@ -1,7 +1,10 @@
-package org.example;
+package org.example.ui;
 
 import java.util.Scanner;
 public class UIMenu {
+
+    public static final String[] MONTHS = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
     public static void showMenu(){
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opción deseada");
@@ -48,6 +51,9 @@ public class UIMenu {
             switch (response){
                 case 1:
                     System.out.println("::Book an appointment");
+                    for (int i = 1; i < 4; i++) {
+                        System.out.println(i +". " + MONTHS[i]);
+                    }
                     break;
                 case 2:
                     System.out.println("::My appointments");
