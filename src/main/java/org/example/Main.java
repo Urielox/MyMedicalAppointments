1ea57f8e0dbb3daf881828469af03fe2715c049c
Urielox <43377310+Urielox@users.noreply.github.com>
2024-10-10 20:14:22 -0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,54 @@
-package org.example;
+package org.example.models;
 
-import org.example.ui.UIMenu;
+public class Patient extends User {
+    //Atributos
+    private String birthday;
+    private double weight;
+    private double height;
+    private String blood;
 
-public class Main {
-    public static void main(String[] args) {
+    Patient (String name, String email){
+        super(name, email);
+    }
 
-        //UIMenu.showMenu();
+    // 54.5
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 
-        Doctor myDoctor = new Doctor("Uriel","Pedriatria");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+    // 54.5 Kg. String
+    public String getWeight(){
+        return this.weight + " Kg.";
+    }
 
-        Patient myPatient = new Patient("Alejandra", "alejandra@gmail.com");
-        System.out.println(myPatient.getweight());
 
+    public String getHeight() {
+        return height + " Mts.";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "\nAge: " + birthday + "\nWeight: " + getWeight() + "\nHeight: " + getHeight() + "\nBlood: "+blood;
     }
 }
